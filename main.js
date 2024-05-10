1f465d19c88da0a74dea8e49bd09f0c1943dcc97
ANIME001ff <165377015+ANIME001ff@users.noreply.github.com>
2024-05-10 15:34:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,126 @@
+
+// let names1 = 'Ali';
+// let names2 = 'Akbar';
+// let names3 = 'Anvar';
+// let names4 = 'Islom';
+// let names5 = 'Husniddin';
+// let names6 = 'husen';
+// let names7 = 'Jonibek';
+// let names8 = 'Akobir';
+// let names9 = 'Shoxrux';
+// let names10 = 'Samandar';
+
+// console.log(names1)
+// console.log(names2)
+// console.log(names3)
+// console.log(names4)
+// console.log(names5)
+// console.log(names6)
+// console.log(names7)
+// console.log(names8)
+// console.log(names9)
+// console.log(names10)
+
+// let age = 23
+// let age2 = 44
+// let age3 = 53
+// let age4 = 45
+// let age5 = 74
+// console.log(age, age2, age3, age4, age5)
+
+// let text = "hello";
+// let text2 = "world";
+// console.log(text + text2)
+// console.log(text - text2)
+// console.log(text * text2)
+// console.log(text / text2)
+
+// let num1 = 2;
+// let num2 = 4;
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+
+// let a = 12;
+// let b = "12";
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+
+// let a = 3;
+// let b = "hi";
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+
+// let a = 4
+// let b = 2
+// console.log(a**3);
+// console.log(b**3);
+
+// let a = 3;
+// let b = 4 * a;
+// console.log(b);
+
+// let a = 2;
+// let S = a ** 2;
+// console.log(S);
+
+// let a = 2;
+// let b = 3
+// let S = a * b;
+// let P = 2 * (a + b)
+// console.log(S);
+// console.log(P);
+
+// let d = 5;
+// let p = 3.14;
+// let L = p * d;
+// console.log(L);
+
+// let a = 5 ** 3;
+// let V = a;
+// let num = 6;
+// let a2 = 4;
+// let S = 6 * (a ** 2);
+// console.log(V);
+// console.log(S);
+
+// let a = 5;
+// let b = 3;
+// let c = 7;
+// let V = a * b * c;
+// let S = 2 * (a*b+b* c+a*c);
+// console.log(V);
+// console.log(S);
+
+// let p = 5;
+// let r = 5;
+// let V = 2*p*r;
+// let S = p*(r**2);
+// console.log(V);
+// console.log(S);
+
+// let a = 5;
+// let b = 5;
+// let S = (a+b)/2;
+// console.log(S);
+
+// let a = 15;
+// let b = 7;
+// let V = (a*b)**0.5;
+// console.log(V);
+
+let a = 5;
+let b = 4;
+let V = a+b;
+let S = a*b;
+let C = a**2;
+let X = b**2;
+console.log(V);
+console.log(S);
+console.log(C);
+console.log(X);
